c97f849681197f2f50116c7b7b8f9a02d393cef1
Joel Kaasinen <joel.kaasinen@gmail.com>
2021-12-15 16:09:00 +0200

    polish day 15

--- Day15.hs
+++ Day15.hs
@@ -9,6 +9,7 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Char
 import Data.Ord
+import Data.Maybe
 
 type Graph = M.Map (Int,Int) Int
 
@@ -20,22 +21,22 @@ example = parse <$> readFile "example.15"
 
 neighbours (x,y) = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
 
-findPath :: Graph -> (Int,Int) -> Graph -> S.Set (Int,Int) -> Graph
-findPath g end distances wavefront
-  | M.member end distances = distances
-  | otherwise = {-trace ("w "++show wavefront) $-} findPath g end distances' wavefront'
+dijkstra :: Graph -> (Int,Int) -> Graph -> S.Set (Int,Int) -> Int
+dijkstra g end distances wavefront
+  | M.member end distances = distances M.! end
+  | otherwise = dijkstra g end distances' wavefront'
   where target = minimumBy (comparing (distances M.!)) (S.elems wavefront)
         targetD = distances M.! target
-        nexts = filter (`M.member`g) $ neighbours target
-        shouldAdd c = (targetD + g M.! c) < M.findWithDefault 99999 c distances
-        nexts' = filter shouldAdd nexts
-        distances' = M.union (M.fromList [(c,targetD + g M.! c) | c <- nexts']) distances
-        wavefront' = S.delete target wavefront `S.union` (S.fromList nexts')
+        nexts = [(c,newD)
+                | c <- neighbours target
+                , delta <- maybeToList $ M.lookup c g
+                , let newD = targetD + delta
+                , newD < M.findWithDefault 99999 c distances]
+        distances' = M.union (M.fromList nexts) distances
+        wavefront' = S.delete target wavefront `S.union` (S.fromList [c | (c,_) <- nexts])
 
-part1 g = final
-  where dists = findPath g end (M.singleton (0,0) 0) (S.singleton (0,0))
-        (end,_) = M.findMax g
-        (_,final) = M.findMax dists
+part1 g = dijkstra g end (M.singleton (0,0) 0) (S.singleton (0,0))
+  where (end,_) = M.findMax g
 
 embiggen g = M.unions [transp i j g | i <- [0..4], j <- [0..4]]
   where transp i j g = M.fromList [((x+w*i,y+h*j),mod (d+i+j-1) 9 +1) | ((x,y),d) <- M.assocs g]
@@ -43,11 +44,9 @@ embiggen g = M.unions [transp i j g | i <- [0..4], j <- [0..4]]
         w = maxx+1
         h = maxy+1
 
-part2 g = final
+part2 g = dijkstra big end (M.singleton (0,0) 0) (S.singleton (0,0))
   where big = embiggen g
         (end,_) = M.findMax big
-        dists = findPath big end (M.singleton (0,0) 0) (S.singleton (0,0))
-        (_,final) = M.findMax dists
 
 main = do
   i <- input
