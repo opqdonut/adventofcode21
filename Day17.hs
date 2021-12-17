04b5cd7ca0a649f49198e429f77646f374093b67
Joel Kaasinen <joel.kaasinen@gmail.com>
2021-12-17 08:03:49 +0200

    day 17 properly

--- Day17.hs
+++ Day17.hs
@@ -1,25 +1,55 @@
 module Day17 where
 
+import Debug.Trace
 import Data.List
 
+type Coord = (Int,Int)
+type Box = (Coord,Coord)
+
+input :: Box
 input = ((56,76),(-162,-134))
 
+example :: Box
 example = ((20,30),(-10,-5))
 
+step :: Coord -> Coord -> Coord
 step (x,y) (velx,vely) = (x+velx,y+vely)
+
+nextVel :: Coord -> Coord
 nextVel (velx,vely) = (velx-signum velx,vely-1)
 
-trajectory vel = scanl' step (0,0) (iterate nextVel vel)
+data Result = Hit Int | Miss | TooHigh
+  deriving (Show,Eq)
 
-hit ((boxx0,boxx1),(boxy0,boxy1)) vel = any ok prefix
-  where prefix = takeWhile (\(x,y) -> y>=boxy0 && x<=boxx1) $ trajectory vel
-        ok (x,y) = and [x >= boxx0, x <= boxx1, y >= boxy0, y <= boxy1]
+inBox :: Box -> Coord -> Bool
+inBox ((boxx0,boxx1),(boxy0,boxy1)) (x,y) =
+  and [x >= boxx0, x <= boxx1, y >= boxy0, y <= boxy1]
 
-increasing as = map snd $ takeWhile (uncurry (<)) $ zip as (tail as)
+simulate box@((boxx0,boxx1),(boxy0,boxy1)) vel = go 0 vel (0,0)
+  where go maxY vel (x,y)
+          | x > boxx1 && y > boxy1 = TooHigh
+          | y < boxy0 = Miss
+          | inBox box (x,y) = Hit maxY
+          | otherwise = go (max maxY y) (nextVel vel) (step (x,y) vel)
 
-maxy vel = maximum $ increasing $ ys
-  where ys = map snd (trajectory vel)
+-- the trajectory is symmetric above the y=0 line, so we know we'll come down via the coordinates
+-- y = vy, 0, -vy-1, -2vy-1, -3vy-1, ...
 
-part1 box = maximum [maxy (vx,vy) | vy <- [1..200], vx <- [1..50], hit box (vx,vy)]
+velBounds ((boxx0,boxx1),(boxy0,boxy1)) =
+  ((1,boxx1),(boxy0,maxYVel))
+  where maxYVel = maximum $ filter ok [1..(-boxy0)+1]
+        ok y = any (<=boxy1) $ takeWhile (>= boxy0) [-k*y-1 | k<-[1..]]
 
-part2 box = length [(vx,vy) | vy <- [-200..1000], vx <- [1..76], hit box (vx,vy)]
+hits box = go vxMin vyMin
+  where ((vxMin,vxMax),(vyMin,vyMax)) = velBounds box
+        go vx vy
+          | vy>vyMax = []
+          | vx>vxMax = go 1 (vy+1)
+          | otherwise = case simulate box (vx,vy) of
+                          Hit y -> y : go (vx+1) vy
+                          Miss -> go (vx+1) vy
+                          TooHigh -> go 1 (vy+1)
+
+part1 box = maximum $ hits box
+
+part2 box = length $ hits box
