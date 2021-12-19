cbda8cc8a3ed5e82b7dc3f0fc421c92a56dc5f10
Joel Kaasinen <joel.kaasinen@gmail.com>
2021-12-19 13:22:20 +0200

    polish day 19

--- Day19.hs
+++ Day19.hs
@@ -25,7 +25,7 @@ neg (a,b,c) = (-a,-b,-c)
 dist :: Coord -> Coord -> Coord
 dist (x,y,z) (x',y',z') = (abs (x-x'),abs (y-y'),abs (z-z'))
 
--- regularized vector distance: ignores rotations and flips
+-- Regularized vector distance: ignores rotations and flips
 regDist :: Coord -> Coord -> Coord
 regDist u v = (a,b,c)
   where (dx,dy,dz) = dist u v
@@ -34,18 +34,18 @@ regDist u v = (a,b,c)
 manhattan :: Coord -> Coord -> Int
 manhattan u v = dot (1,1,1) $ dist u v
 
--- transform p to the coordinate system where x points to `facing` and y points to `up`
+-- Transform p to the coordinate system where x points to `facing` and y points to `up`
 rotation :: Coord -> Coord -> Coord -> Coord
 rotation facing up p = (x,y,z)
   where x = dot facing p
         y = dot up p
         z = dot (cross facing up) p
 
--- all the major axes and their negations
+-- All the major axes and their negations
 directions :: [Coord]
 directions = [(1,0,0),(0,1,0),(0,0,1),(-1,0,0),(0,-1,0),(0,0,-1)]
 
--- all possible combinations of 90° rotations around axes
+-- All possible combinations of 90° rotations around axes
 rotations :: [Coord -> Coord]
 rotations = [rotation facing up | facing <- directions, up <- directions, dot up facing == 0]
 
@@ -64,11 +64,11 @@ parse i = map scanner parts
 input = parse <$> readFile "input.19"
 example = parse <$> readFile "example.19"
 
--- which rotations map {b1,b2} to {a1,a2}?
+-- Which rotations map {b1,b2} to {a1,a2}?
 rotationsMatching a1 a2 b1 b2 = [f | f <- rotations, delta == dist (f b1) (f b2)]
   where delta = dist a1 a2
 
--- which transforms map {b1,b2} to {a1,a2}?
+-- Which transforms map {b1,b2} to {a1,a2}?
 transformsMatching a1 a2 b1 b2 =
   [ add t . r
   | r <- rotationsMatching a1 a2 b1 b2
@@ -77,11 +77,11 @@ transformsMatching a1 a2 b1 b2 =
         b = min (r b1) (r b2)
         t = add (neg b) a]
 
--- do two scanners overlap enough?
+-- Do two scanners overlap enough?
 overlaps :: Scanner -> Scanner -> Bool
 overlaps sa sb = length (intersect sa sb) >= 12
 
--- find the transforms f mapping {pb1,pb2} to {pa1,pa2}
+-- Find the transforms f mapping {pb1,pb2} to {pa1,pa2}
 -- such that f sb overlaps with sa
 matching :: Coord -> Coord -> Coord -> Coord -> Scanner -> Scanner -> [Coord -> Coord]
 matching pa1 pa2 pb1 pb2 sa sb =
@@ -95,7 +95,11 @@ regDists :: Scanner -> M.Map Coord (Coord,Coord)
 regDists scanner = M.fromList [ (regDist u v,(u,v))
                               | (u:rest) <- tails scanner, v <- rest]
 
--- find a transform that matches sb to sa
+-- Find a transform that matches sb to sa.
+--
+-- Avoids comparing all possible pairs of points by trying to match
+-- pairs pa1,pa2 <- sa with pairs pb1,pb2 <- sb only when
+--   regDist pa1 pa2 == regDist pb1 pb2
 findFit :: Scanner -> Scanner -> Maybe (Coord -> Coord)
 findFit sa sb = listToMaybe rets
   where ra = regDists sa
@@ -110,7 +114,7 @@ fitAll' origins beacons (scanner:scanners) =
     Just f -> fitAll' (origins++[f (0,0,0)]) (S.union beacons (S.fromList (map f scanner))) scanners
     Nothing -> fitAll' origins beacons (scanners++[scanner])
 
--- match all scanners together, returning scanner locations and beacon locations
+-- Match all scanners together, returning scanner locations and beacon locations
 fitAll :: [Scanner] -> ([Coord],S.Set Coord)
 fitAll (s:ss) = fitAll' [(0,0,0)] (S.fromList s) ss
 
